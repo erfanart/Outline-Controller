8c4e103ab91ce83b6d3fbd5acb8b236c46b6b61f
erfanart <erfanansary313@gmail.com>
2024-09-20 19:01:42 +0000

    Version2

---
+++ manager/bot/send_bot.py
@@ -0,0 +1,9 @@
+import requests
+# import sys
+
+def send_to_telegram(message,ApiURL,ChatID):
+    try:
+        response = requests. post(ApiURL, json={'chat_id': ChatID, 'text': message})
+        print(response.text)
+    except Exception as e:
+        print(e)
